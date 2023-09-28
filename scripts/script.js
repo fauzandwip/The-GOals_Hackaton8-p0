4d8c29685dabfd02f25c34a24d2f8043bc622121
fauzandwip <fauzan27dp@gmail.com>
2023-09-28 20:37:51 +0700

    create detail.html create detail-style.css create detail-script.js add hour configuration to database

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,19 @@
-const database = [
+export const database = [
 	{
 		id: 1,
 		name: "Fortnite",
 		type: "pc",
 		targetTime: 22,
 		reachTime: 10,
+		urlImage: "/assets/fortnite-img.jpg",
+		urlGame: "https://www.fortnite.com/",
+	},
+	{
+		id: 2,
+		name: "Mobile Legends",
+		type: "mobile",
+		targetTime: 24,
+		reachTime: 12,
 		urlImage: "/assets/fortnite-img.jpg",
 		urlGame: "https://www.fortnite.com/",
 	},
