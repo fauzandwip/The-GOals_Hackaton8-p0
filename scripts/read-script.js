59a5706d4e18d41a05310a4ec7ae8c86a28ffc98
fauzandwip <fauzan27dp@gmail.com>
2023-09-28 21:25:07 +0700

    configuration read data from js to html homepage

---
+++ scripts/read-script.js
@@ -0,0 +1,23 @@
+import { database } from "./script.js";
+
+function load() {
+	const container = document.getElementsByClassName("container")[0];
+	console.log("trigerr load");
+	database.forEach((element) => {
+		const { name, urlImage } = element;
+
+		container.innerHTML += `<div class="box">
+				<div class="dots">
+					<button><img src="./assets/more.png" /></button>
+					<div>
+						<button><img src="./assets/edit.png" /></button>
+						<button><img src="./assets/delete.png" /></button>
+					</div>
+				</div>
+				<img id="img-game" src="${urlImage}" />
+				<h2>${name}</h2>
+			</div>`;
+	});
+}
+load();
+// console.log("trigerr");
